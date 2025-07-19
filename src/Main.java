e2e696f3c675a763666ec727ebe8311fe31725f2
Иван <ivan.shutov95@mail.ru>
2025-07-19 10:56:58 +0300

    WH1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Задача 1");
+        int age = 24;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен"+ age +"он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен"+ age + "он не достиг совершеннолетия, нужно немного подождать");
+        }
+
+        }
+}
